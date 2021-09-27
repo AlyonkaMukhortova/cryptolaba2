f05ea105cc547c9be3011645f468e75e65b70764
Alena <alyonka.mukhortova@gmail.com>
2021-09-27 03:03:46 +0300

    Add files for debug

---
+++ debug.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "debug.h"
+
+
+const char* info[] = {"Block before crypt", "Block after crypt", "Block after XOR with KEY", "Block after S-BLOCK", "Block after SHIFT", "Block after XOR with IV"};
+
+
+Debug* init(int blocks){
+  Debug* new = (Debug*)malloc(sizeof(Debug));
+  new->blocks_num = blocks;
+  new->key = (unsigned int*)malloc(3 * sizeof(unsigned int));
+  new->real_num = 0;
+  new->all = (unsigned int*)malloc(14 * blocks);
+  return new;
+}
+
+
+void print_debug(Debug* debug, int crypt_mode, char mode){
+  int mess[7] = {0, 2, 3, 4, 2, 1};
+  int num_mes, ind;
+  printf("KEY0:   %x\n", debug->skey);
+  printf("KEY1:   %x\n", debug->key[0]);
+  printf("KEY2:   %x\n", debug->key[1]);
+  if (crypt_mode == 1 && mode == 'e'){
+    num_mes = 6;
+  }
+  else if (crypt_mode == 2 && mode == 'e'){
+    num_mes = 6;
+    mess[2] = 2;
+    mess[3] = 4;
+    mess[4] = 3;
+  }
+  else if (crypt_mode == 1 && mode == 'c'){
+    printf("IV:   %x\n", debug->iv);
+    num_mes = 7;
+    mess[2] = 5;
+    mess[3] = 3;
+    mess[4] = 4;
+    mess[5] = 2;
+    mess[6] = 1;
+  }
+  else if (crypt_mode == 2 && mode == 'c'){
+    printf("IV:   %x\n", debug->iv);
+    num_mes = 7;
+    mess[3] = 4;
+    mess[4] = 3;
+    mess[5] = 5;
+    mess[2] = 2;
+    mess[6] = 1;
+  }
+  printf("%d\n", debug->real_num);
+  for(int i = 0; i < debug->real_num; i++){
+    ind = i % num_mes;
+    printf("%d\n", ind);
+    printf("%s:   %x\n", info[mess[ind]], debug->all[i]);
+  }
+}
+
+
+void delete_debug();
---
+++ debug.h
@@ -0,0 +1,20 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+
+typedef struct Debug{
+  int real_num;
+  int blocks_num;
+  unsigned int iv;
+  unsigned int* all;
+  unsigned int skey; //start key
+  unsigned int * key; //round keys
+}Debug;
+
+
+Debug* init(int blocks);
+void print_debug(Debug* debug, int crypt_mode, char mode);
+void delete_debug();
+
+
+#endif //DEBUG_H
